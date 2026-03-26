977be50d5028551186ec1a8b91ef4fd648df5842
bogdan999chaus <chaus0550@gmail.com>
2026-03-26 17:23:09 +0200

    Add: game proto

--- main.go
+++ main.go
@@ -1,5 +1,50 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"math/rand"
+	"strconv"
+	"time"
+)
 
+const totalPoints = 100
+const pointsperQuestion = 20
+
+func main() {
+	fmt.Println("Вітаю у грі Богдана Чауса \\m/")
+
+	for i := 5; i > 0; i-- {
+		fmt.Println(i)
+		time.Sleep(1 * time.Second)
+
+	}
+
+	fmt.Println("Good luck!")
+	myPoints := 0
+	start := time.Now()
+
+	for myPoints < totalPoints {
+		x, y := rand.Intn(100), rand.Intn(100)
+
+		fmt.Printf("%v + %v = ", x, y)
+
+		ans := ""
+		fmt.Scan(&ans)
+
+		ansInt, err := strconv.Atoi(ans)
+		if err != nil {
+			fmt.Println("НЕПРАВИЛЬНО!!!")
+		} else {
+			if ansInt == x+y {
+				myPoints += pointsperQuestion
+				fmt.Printf("Правильно! Кількість балів: %v\n", myPoints)
+			} else {
+				fmt.Println("Спробуй ще!")
+			}
+		}
+	}
+
+	end := time.Now()
+	timeSpent := end.Sub(start)
+	fmt.Printf("Вітаю! Ти впорався(лась) за %v!", timeSpent)
 }
